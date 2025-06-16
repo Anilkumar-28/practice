608519c0be8d85bb809774cd1ffcd49f0e359815
KILARI ANIL KUMAR <141000974+Anilkumar-28@users.noreply.github.com>
2025-06-16 17:53:16 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,13 @@
+// var num1=34
+// var num2=56
+// var sum=num1+num2
+// console.log("Sum of Two Numbers is :", sum)
+
+// console.log(typeof(true))
+// var name="Anil"
+// var name="kumar"
+// console.log(name)
+console.log(typeof(10 - "A"))
+
+console.log(parseInt("10")+parseInt("5"))
+console.log(typeof(10 * "5"))
